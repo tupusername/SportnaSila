a3736b43cddb7f11e50a12475377021287f9158d
tupusername <manoimilov@gmail.com>
2026-02-11 12:03:04 +0200

    first

---
+++ SportnaSila/Models/Orders.cs
@@ -0,0 +1,15 @@
+﻿namespace SportnaSila.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public string ClientId { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public DateTime DateOrder { get; set; }
+
+        public Clients Client { get; set; }
+        public ICollection<Products> Products { get; set; }
+    }
+}
---
+++ SportnaSila/Models/Products.cs
@@ -0,0 +1,22 @@
+﻿namespace SportnaSila.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public int CategoryId { get; set; }
+        public int SupplierId { get; set; }
+        public int BrandId { get; set; }
+        public int OrderId { get; set; }
+
+        public Categories Category { get; set; }
+        public Suppliers Supplier { get; set; }
+        public Brands Brand { get; set; }
+        public Orders Order { get; set; } = new Orders();
+    }
+}
